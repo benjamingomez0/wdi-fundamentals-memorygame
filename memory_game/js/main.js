d7ad73552cd81c68c7648636b7f7278bbc583118
Benjamin Gomez <benjamingomez@Benjamins-MacBook-Pro.local>
2019-09-06 15:59:14 -0700

    created game logic with functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,27 +2,32 @@
 const cards = ["queen","queen","king","king"];
 //cards in play array
 const cardsInPlay = [];
-//card choice 
-let cardOne = cards[0];
-//add choice to cards in play array
-cardsInPlay.push(cardOne);
-//console check
-console.log("User flipped " + cardOne);
-//card 2 choice
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-//add choice to cards in play array
-console.log("User flipped " + cardsInPlay[1]);
 
-//outcome logic
-if(cardsInPlay.length == 2)
-{
+//outcome function
+function checkForMatch(){
+	
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
-		alert("You found a match!");
+		console.log("You found a match!");
 	}
-	else if(cardsInPlay[0] !== cardsInPlay[1])
+	else 
 	{
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
+	}
+	
+}
+
+function flipCard(cardId){
+
+	console.log("User flipped: " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length == 2)
+	{
+		checkForMatch();
 	}
 }
+flipCard(0);
+flipCard(2);
+
+
+
