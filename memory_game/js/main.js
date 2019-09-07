ba7980b3bdf356a8ee8a4b2f672d2abd2c2067f2
Benjamin Gomez <benjamingomez@Benjamins-MacBook-Pro.local>
2019-09-07 16:11:18 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,27 @@
 //cards array
-const cards = ["queen","queen","king","king"];
+const cards = [
+				{
+					rank: "queen",
+					suit: "hearts",
+					cardImage:"images/queen-of-hearts.png"
+				},
+				{
+					rank: "queen",
+					suit: "diamonds",
+					cardImage:"images/queen-of-diamonds.png"
+				},
+				{
+					rank: "king",
+					suit: "hearts",
+					cardImage:"images/king-of-hearts.png"
+				},
+				{
+					rank: "king",
+					suit: "diamonds",
+					cardImage:"images/king-of-diamonds.png"
+				}
+
+];
 //cards in play array
 const cardsInPlay = [];
 
@@ -9,25 +31,32 @@ function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1])
 	{
 		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else 
 	{
 		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 	
 }
 
 function flipCard(cardId){
 
-	console.log("User flipped: " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped: " + cards[cardId].rank);
+	//display suit and img
+	console.log("suit: " + cards[cardId].suit);
+	console.log("image: " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length == 2)
 	{
 		checkForMatch();
 	}
+
 }
 flipCard(0);
 flipCard(2);
+
 
 
 
